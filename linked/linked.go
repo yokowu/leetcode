0619eee847c74969b5e5e8c62e408e7417ddedfd
Yoko <18836617@qq.com>
2022-04-06 22:11:43 +0800

    linked delete duplicates

--- linked/linked.go
+++ linked/linked.go
@@ -279,3 +279,24 @@ func RotateRight2(head *ListNode, k int) *ListNode {
 	fast.Next = head
 	return newHead
 }
+
+func DeleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	cur := head
+	for cur != nil {
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur = cur.Next
+		}
+		if pre.Next == cur {
+			pre = pre.Next
+		} else {
+			pre.Next = cur.Next
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
--- main.go
+++ main.go
@@ -10,13 +10,21 @@ func main() {
 	root := linked.New(1)
 	root = linked.Append(root, 2)
 	root = linked.Append(root, 3)
+	root = linked.Append(root, 3)
 	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 4)
+	root = linked.Append(root, 5)
 	root = linked.Append(root, 5)
 	root = linked.Append(root, 6)
 
 	linked.Show(root)
 
 	fmt.Println("-----")
-	root = linked.RotateRight2(root, 2)
+	root = linked.DeleteDuplicates(root)
 	linked.Show(root)
 }
