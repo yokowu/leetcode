4a6e624def13610e7eb86361658f53319cd99f79
Yoko <18836617@qq.com>
2022-04-07 21:12:46 +0800

    linked partition

--- linked/linked.go
+++ linked/linked.go
@@ -300,3 +300,21 @@ func DeleteDuplicates(head *ListNode) *ListNode {
 	}
 	return dummy.Next
 }
+
+func Partition(head *ListNode, x int) *ListNode {
+	small, large := &ListNode{}, &ListNode{}
+	smallH, largeH := small, large
+	for head != nil {
+		if head.Val < x {
+			small.Next = head
+			small = small.Next
+		} else {
+			large.Next = head
+			large = large.Next
+		}
+		head = head.Next
+	}
+	small.Next = largeH.Next
+	large.Next = nil
+	return smallH.Next
+}
