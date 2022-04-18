8dbcb4d988317b957158d3d7458c5c7a70d583b7
Yoko <18836617@qq.com>
2022-04-18 11:27:12 +0800

    linked reverse between 2

--- linked/linked.go
+++ linked/linked.go
@@ -353,3 +353,20 @@ func reverse(head *ListNode) {
 		cur = next
 	}
 }
+
+func ReverseBetween2(head *ListNode, left, right int) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = pre.Next
+		pre.Next = next
+	}
+	return dummy.Next
+}
