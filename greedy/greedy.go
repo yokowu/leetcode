493fe4b5f84db58197b3fc6ecb25c2d78d448bcf
Yoko <18836617@qq.com>
2022-06-20 20:48:29 +0800

    greedy findcontentchildren

--- greedy/greedy.go
+++ greedy/greedy.go
@@ -1,5 +1,7 @@
 package greedy
 
+import "sort"
+
 func LongestPalindrome(s string) int {
 	chars := make([]int, 128)
 	for _, v := range s {
@@ -13,4 +15,18 @@ func LongestPalindrome(s string) int {
 		res++
 	}
 	return res
+}
+
+func FindContentChildren(g, s []int) int {
+	sort.Ints(s)
+	sort.Ints(g)
+
+	i, j := 0, 0
+	for i < len(g) && j < len(s) {
+		if g[i] <= s[j] {
+			i++
+		}
+		j++
+	}
+	return i
 }
--- main.go
+++ main.go
@@ -3,20 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/yokowu/leetcode/linked"
+	"github.com/yokowu/leetcode/greedy"
 )
 
 func main() {
-	list1 := linked.New(1)
-	list1 = linked.Append(list1, 3)
-	list1 = linked.Append(list1, 2)
-	list1 = linked.Append(list1, 2)
-	list1 = linked.Append(list1, 3)
-	list1 = linked.Append(list1, 2)
-	list1 = linked.Append(list1, 2)
-	list1 = linked.Append(list1, 2)
-	list1 = linked.Append(list1, 7)
+	i := greedy.FindContentChildren(
+		[]int{3, 4, 5},
+		[]int{2, 3, 4},
+	)
 
-	list := linked.NodesBetweenCriticalPoints(list1)
-	fmt.Println(list)
+	fmt.Println(i)
 }
